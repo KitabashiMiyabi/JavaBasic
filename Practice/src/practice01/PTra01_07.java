658ff73c496527c2178e7273225155d2cbb5fea8
DESKTOP-HCQ37GF\tikit <miyabi.koitabashi.likeit@gmail.com>
2018-09-12 18:28:19 +0900

    1-7

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -10,19 +10,25 @@ public class PTra01_07 {
 	public static void main(String[] args) {
 
 		// 基本型8種類の変数を宣言します
-		boolean bo;
-		byte by;
-		short s;
-		char c;
-		int i;
-		long l;
-		float f;
-		double d;
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
 
-
+		boolean isGetUp;
+        isGetUp = false;
+        System.out.println(isGetUp);
+		short s = 10;
+		System.out.println(s);
+		char c = 'R';
+        System.out.println(c);
+		int i = 10;
+	    System.out.println(i);
+		long l = 100000000L;
+	    System.out.println(l);
+		float f = 3.14F ;
+        System.out.println(f);
+		double d = 3.5 ;
+        System.out.println(d);
 
 
 
